bfe713c3ef168dfc35fe8ef92afffd65e1a03dee
mackap70@gmail.com <mackap70@gmail.com>
2018-03-30 14:59:29 +0300

    hashKey

---
+++ AppUtils.java
@@ -0,0 +1,26 @@
+public class AppUtils {
+
+  public static String getHashKey
+  String hashKeyStr = null;
+    try {
+    PackageInfo info = getPackageManager().getPackageInfo(
+        getApplicationContext().getPackageName(),
+        PackageManager.GET_SIGNATURES);
+    for (Signature signature : info.signatures) {
+      MessageDigest md = MessageDigest.getInstance("SHA");
+      md.update(signature.toByteArray());
+      hashKeyStr = Base64.encodeToString(md.digest(), Base64.DEFAULT);
+    }
+  } catch(
+  NameNotFoundException e)
+
+  {
+
+  } catch(
+  NoSuchAlgorithmException e)
+
+  {
+
+  }
+return hashKeyStr;
+}
